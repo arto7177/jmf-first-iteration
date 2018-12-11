215fece0096998535ebfd6859d157ce0059fd224
Brian Lynch <bijil6339@gmail.com>
2018-12-11 12:54:38 -0500

    everything was broken, starting from scratch, keep it simple

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
-require 'index.view.php';
+$greeting = "Hello World!";
 
-new PDO('mysql:host=127.0.01;dbname=donees', 'root', '');
+$pdo = new PDO('mysql:host=127.0.0.1;dbname=jmffoundation', 'root', '');
+
+require 'index.view.php';
