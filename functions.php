74b2ce270230fc8961265843200a2388dc4bed19
Brian Lynch <bijil6339@gmail.com>
2018-12-11 14:44:47 -0500

    cleaned up code and file structure a bit

--- functions.php
+++ functions.php
@@ -6,3 +6,26 @@ function dieanddump($data)
 	die(var_dump($data));
 	echo '</pre>';
 }
+//Function to test output of small snippets of code
+
+function connectToDb()
+{
+	try{
+
+	return new PDO('mysql:host=127.0.0.1;dbname=jmffoundation', 'root', '');
+} catch (PDOException $e) {
+	die($e->getMessage());
+}
+}
+//Function to connect to database using PDO
+
+
+function fetchAllDonees($pdo) {
+	
+$statement = $pdo->prepare('select * from donees');
+
+$statement->execute();
+
+return $statement->fetchAll(PDO::FETCH_CLASS, 'Donees');
+}
+//Return list of all Donees in database
--- index.php
+++ index.php
@@ -2,19 +2,13 @@
 
 $greeting = "Hello World!";
 
+require 'donees.php';
+
 require 'functions.php';
 
-try{
+$pdo = connectToDb();
 
-	$pdo = new PDO('mysql:host=127.0.0.1;dbname=jmffoundation', 'root', '');
-} catch (PDOException $e) {
-	die('Could not connect');
-}
+$donees =fetchAllDonees($pdo);
 
-$statement = $pdo->prepare('select * from donees');
-
-$statement->execute();
-
-var_dump($statement->fetchAll(PDO::FETCH_OBJ));
 
 require 'index.view.php';
