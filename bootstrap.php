820230ba27d5a7bfe568bdb03fb1ca30ef9b9c64
Brian Lynch <bijil6339@gmail.com>
2018-12-11 18:26:06 -0500

    refactored connection for increased security

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,8 @@
 <?php
+$config = require 'config.php';
+
 require 'database/connection.php';
 
 require 'database/querybuilder.php';
 
-return new QueryBuilder(Connection::make());
+return new QueryBuilder(Connection::make($config['database']));
